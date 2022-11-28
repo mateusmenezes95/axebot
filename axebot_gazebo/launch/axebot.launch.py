2957a770116228feed5883b01909547b0574b7b7
Mateus Menezes <mateus.meneses@fieb.org.br>
2022-11-27 23:08:40 -0300

    Put axebot name in gazebo spawn

--- axebot_gazebo/launch/axebot.launch.py
+++ axebot_gazebo/launch/axebot.launch.py
@@ -75,7 +75,7 @@ def generate_launch_description():
 
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                     arguments=['-topic', 'robot_description',
-                                '-entity', 'my_bot'],
+                                '-entity', 'axebot'],
                     output='screen'
     )
 
